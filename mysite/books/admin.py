f2ceb3890a4d06b9bf74af6aafb01e7b03f89aa7
unknown <qwantilium>
2022-10-09 13:35:45 +0500

    templates, migrations

--- mysite/books/admin.py
+++ mysite/books/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'date', 'publisher', 'isbn',)
+    search_fields = ("title",)
+    list_filter = ("author",)
+    empty_value_display = "-пусто-"
+
+
+admin.site.register(Book, BookAdmin)
--- mysite/books/urls.py
+++ mysite/books/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('book_list', views.book_list, name='book_list'),
-    path('new_book/', views.new_book, name='new_book'),
+    path('', views.book_list, name='new_book'),
+    path('book_list/', views.new_book, name='book_list'),
 ]
